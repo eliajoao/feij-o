debbbf016c769839bc2988bc7064e499e3831be5
eliajoao <168561679+eliajoao@users.noreply.github.com>
2024-09-23 14:20:10 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Esporte',
+    'Quem foi o time campeão do mundial de clubes de 1981?',
+    'O time campeão do mundial é o flamengo'
+)
+
+criaCartao(
+    'lingua Inglesa',
+    'Como falar porque está tão sério em inglês?',
+    'Why are you so serious?'
+)
+
+criaCartao(
+    'Esporte',
+    'Quem é o atual campeão da libertadores?',
+    'o campeão da libertadores é o fluminense'
+)
+
+criaCartao(
+    'console',
+    'qual foi o console mais famoso no japão?',
+    'Nintendo Switch'
+)
